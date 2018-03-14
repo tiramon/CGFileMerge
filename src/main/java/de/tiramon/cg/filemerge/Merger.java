719173db759fc3d56875da2c934ddec16d0c653f
tiramon <github@checkspam.de>
2018-03-14 08:29:30 +0100

    adding current version to first line

--- src/main/java/de/tiramon/cg/filemerge/Merger.java
+++ src/main/java/de/tiramon/cg/filemerge/Merger.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -50,6 +51,7 @@ public class Merger {
 		System.err.println("writing to " + outputFile.getAbsolutePath());
 
 		try (PrintStream ps = new PrintStream(outputFile)) {
+			ps.println("//Version " + new Date());
 			ps.print(content);
 		}
 	}
